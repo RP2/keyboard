0726e84282aace23d6b9da5b38323580fef0b6c9
RP2 <RP2@users.noreply.github.com>
2022-03-30 16:28:56 -0700

    bakeneko review
    rev 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,82 @@
-$.fn.moveIt = function(){
-    var $window = $(window);
-    var instances = [];
-    
-    $(this).each(function(){
-      instances.push(new moveItItem($(this)));
-    });
-    
-    window.addEventListener('scroll', function(){
-      var scrollTop = $window.scrollTop();
-      instances.forEach(function(inst){
-        inst.update(scrollTop);
-      });
-    }, {passive: true});
-  }
-  
-  var moveItItem = function(el){
-    this.el = $(el);
-    this.speed = parseInt(this.el.attr('data-scroll-speed'));
-  };
-  
-  moveItItem.prototype.update = function(scrollTop){
-    this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
-  };
-  
-  // Initialization
-  $(function(){
-    $('[data-scroll-speed]').moveIt();
+$.fn.moveIt = function () {
+  var $window = $(window);
+  var instances = [];
+
+  $(this).each(function () {
+    instances.push(new moveItItem($(this)));
   });
 
-  // click link with hash scrolls to div with matching id
-$('a[href*="#"]')
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function (event) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-                left: target.offset().left,
-                scrollTop: target.offset().top
-            }, 1000, function () {
-                var $target = $(target);
-                $target.focus();
-                if ($target.is(":focus")) {
-                    return false;
-                } else {
-                    $target.attr('tabindex', '-1');
-                    $target.focus();
-                };
-            });
-        }
-    }
+  window.addEventListener('scroll', function () {
+    var scrollTop = $window.scrollTop();
+    instances.forEach(function (inst) {
+      inst.update(scrollTop);
+    });
+  }, {
+    passive: true
+  });
+}
+
+var moveItItem = function (el) {
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function (scrollTop) {
+  this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+};
+
+// Initialization
+$(function () {
+  $('[data-scroll-speed]').moveIt();
 });
+
+// click link with hash scrolls to div with matching id
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          left: target.offset().left,
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex', '-1');
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
 
 //night mode at night
 let currentTime = new Date().getHours();
-if ($("body")) {
-    if (7 <= currentTime && currentTime < 20) {
-        $("body, nav, a").removeClass("night-time")
+$(document).ready(function () {
+  if (7 <= currentTime && currentTime < 20) {
+    $("body, nav, a").removeClass("night-time")
+  } else {
+    $("body, nav, a, a::before").addClass("night-time")
+  }
+});
+
+//scroll animation stuff
+$(window).scroll(function () {
+  if ($("#about").is(":visible")) {
+    if ($(window).scrollTop() >= $("#about").offset().top - $("header").outerHeight()) {
+      $("#about .paragraph").addClass("slide-left");
+      $("#about h2").addClass("slide-right");
+    } 
+  }else if ($("#disclaimer").is(":visible")){
+      if ($(window).scrollTop() >= $("#bakeneko60-review").offset().top) {
+        $("#bakeneko60-review ul").addClass("slide-right");
+      }
     }
-    else {
-        $("body, nav, a").addClass("night-time")
-    }
-}
+});
